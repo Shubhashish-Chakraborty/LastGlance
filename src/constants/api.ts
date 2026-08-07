cb1083fbe5545e3291d455d20dc3c5715bcf40ab
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2026-08-08 01:34:32 +0530

    prod api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,4 @@
 // ipconfig getifaddr en0
 
-export const API_BASE_URL = 'http://192.168.1.3:3001'; // backend URL!
+// export const API_BASE_URL = 'http://192.168.1.3:3001'; // backend URL!
+export const API_BASE_URL = 'https://lastglanceapi.onrender.com'; // backend URL!
